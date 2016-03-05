5c85e5fb974b94b9adc84f4cb4a1bcdf0035e54e
Lukas Alius <usergarry@gmail.com>
2016-03-05 15:15:11 +0000

    Initial commit

---
+++ app/src/main/java/com/studenthack/team40/team40/Checkpoint.java
@@ -0,0 +1,45 @@
+package com.studenthack.team40.team40;
+
+import android.annotation.SuppressLint;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.Identifier;
+
+@SuppressLint("ParcelCreator")
+public class Checkpoint {
+    private LatLng coordinates;
+    private String name;
+    private double distance;
+    private Identifier id1;
+    private Identifier id2;
+    private Identifier id3;
+
+    public LatLng getCoordinates(){ return coordinates; }
+    public void setCoordinates(LatLng co) { coordinates = co; }
+
+    public String getName() { return name; }
+    public void setName(String n) { name = n; }
+
+    public double getDistance() { return distance; }
+    public void setDistance(double dis) { distance = dis; }
+
+    public Identifier getId1() { return id1; }
+    public void setId1(Identifier id) { id1 = id; }
+
+    public Identifier getId2() { return id2; }
+    public void setId2(Identifier id) { id2 = id; }
+
+    public Identifier getId3() { return id3; }
+    public void setId3(Identifier id) { id3 = id; }
+
+    public Checkpoint(LatLng co, String n, Beacon beacon){
+        coordinates = co;
+        name = n;
+        distance = beacon.getDistance();
+        id1 = beacon.getId1();
+        id2 = beacon.getId2();
+        id3 = beacon.getId3();
+    }
+}
---
+++ app/src/main/java/com/studenthack/team40/team40/MainActivity.java
@@ -0,0 +1,240 @@
+package com.studenthack.team40.team40;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    public static final String TAG = "BeaconsEverywhere";
+    private BeaconManager beaconManager;
+    private TextView txt;
+    private ArrayList<Beacon> listOfBeacons = new ArrayList<>();
+    private ArrayList<Checkpoint> list = new ArrayList<>();
+    private Region region;
+    private GoogleApiClient client;
+    private BeaconConsumer consumer = this;
+    private Boolean running = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (running){
+                    running = false;
+                    beaconManager.unbind(consumer);
+                    Snackbar.make(view, "Stopped", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+
+                } else {
+                    running = true;
+                    beaconManager.bind(consumer);
+                    Snackbar.make(view, "Running", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+
+        Beacon beacon = new Beacon.Builder()
+                .setId1("b9407f30-f5f8-466e-aff9-25556b57fe6d")
+                .setId2("14547")
+                .setId3("61162")
+                .setManufacturer(0x0118) // Radius Networks.  Change this for other beacon layouts
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(new Long[]{0l})) // Remove this for beacon layouts without d: fields
+                .build();
+
+        list.add(new Checkpoint(new LatLng(123.0, 123.0), "Point 1", beacon));
+        region = new Region("ranged region", Identifier.fromUuid(UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D")), null, null);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        beaconManager.bind(this);
+
+        txt = (TextView) findViewById(R.id.mainText);
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private Boolean compare() {
+        for (Beacon beacon : listOfBeacons) {
+            for (Checkpoint c : list) {
+                Log.d(TAG, c.getId3() + " | " + beacon.getId3());
+                if (c.getId3().equals(beacon.getId3())) return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        final Region region = new Region("myBeacons", Identifier.parse("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                try {
+                    Log.d(TAG, "didEnterRegion");
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                try {
+                    Log.d(TAG, "didExitRegion");
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for (Beacon oneBeacon : beacons) {
+
+                    if (listOfBeacons.contains(oneBeacon)) {
+                        listOfBeacons.remove(oneBeacon);
+                        listOfBeacons.add(oneBeacon);
+                    } else listOfBeacons.add(oneBeacon);
+                    Log.d(TAG, "distance: " + oneBeacon.getDistance() + " id:" + oneBeacon.getId1() + "/" + oneBeacon.getId2() + "/" + oneBeacon.getId3());
+                }
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        txt.setText(listOfBeacons.toString());
+                        if (compare()) {
+                            Toast.makeText(getApplicationContext(), "Found a Beacon!", Toast.LENGTH_SHORT).show();
+                            beaconManager.unbind(consumer);
+                            running = false;
+                        }
+                    }
+                });
+
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(region);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.studenthack.team40.team40/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.studenthack.team40.team40/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
